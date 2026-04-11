577ac41b071cb7ed7d301901800ba60a7ce29702
dtl <arpitawagh9146.in>
2026-04-11 16:19:58 +0530

    First commit - Quiz Application

---
+++ Question.java
@@ -0,0 +1,11 @@
+class Question {
+    String question;
+    String[] options;
+    int correctAnswer;
+
+    Question(String question, String[] options, int correctAnswer) {
+        this.question = question;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+        
