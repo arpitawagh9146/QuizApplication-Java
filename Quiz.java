1be08fa983f2614478171fb0576005483f82259c
dtl <arpitawagh9146.in>
2026-04-11 16:39:37 +0530

    First commit - Quiz Application

---
+++ Quiz.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Quiz {
+    List<Question> questions = new ArrayList<>();
+    int score = 0;
+
+    void addQuestion(Question q) {
+        questions.add(q);
+    }
+
+    void startQuiz() {
+        Scanner sc = new Scanner(System.in);
+
+        for (Question q : questions) {
+            System.out.println("\n" + q.question);
+
+            for (int i = 0; i < q.options.length; i++) {
+                System.out.println((i + 1) + ". " + q.options[i]);
+            }
+
+            System.out.print("Enter answer: ");
+            int ans = sc.nextInt();
+
+            if (ans == q.correctAnswer) {
+                score++;
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+
+        System.out.println("\nFinal Score: " + score);
+    }
+}
